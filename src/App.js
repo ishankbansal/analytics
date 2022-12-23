710f34a893d81b76716b2cf3c71d6bf9412bdbb4
Ishank Bansal <ishankbansal1111@gmail.com>
2022-12-23 05:33:11 +0530

    app update

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React from 'react'
 import DateRangeComp from './components/DateRangeComp'
-import SettingsComp from './components/SettingsComp'
 import TableComp from './components/TableComp'
 import DateState from './context/DateState'
+import SettingState from './context/SettingState'
 import "./index.css"
 
 const App = () => {
   return (
     <div>
+      <h1 className="heading">Analytics</h1>
+      <SettingState>
       <DateState>
         <DateRangeComp/>
-        <SettingsComp/>
         <TableComp/>
       </DateState>
+      </SettingState>
     </div>
   )
 }
